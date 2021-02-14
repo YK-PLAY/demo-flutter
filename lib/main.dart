d027c489d4a1c962b3a05a8bfbddb7708621b8d3
Kebron <ztkmkoo@gmail.com>
2021-02-14 22:33:40 +0900

    # update main
    - make hello world app

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Welcome to Flutter',
+      home: Scaffold (
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,8 +31,7 @@ class MyApp extends StatelessWidget {
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      )
     );
   }
 }
