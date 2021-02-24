37d749b26fef8faf7c528ec8a5be4cfcca8b39b1
Kebron <ztkmkoo@gmail.com>
2021-02-25 03:16:01 +0900

    # tutorial 10
    - add new home widget
    - improve login widget

---
+++ lib/login/login.dart
@@ -0,0 +1,100 @@
+import 'dart:collection';
+
+import 'package:demo_flutter/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  static final sessionKey = 'session';
+  final _storage = FlutterSecureStorage();
+  final userMap = new HashMap();
+
+  bool loggedIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    if(loggedIn) {
+      return HomeScreen();
+    } else {
+      return _loginPage();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tryLogin();
+  }
+
+  void _tryLogin() async {
+    final String session = await _storage.read(key: sessionKey);
+    if(_validSession(session)) {
+      loggedIn = true;
+    }
+  }
+
+  bool _validSession(String session) {
+    // TODO - do some network job with server
+    return true;
+  }
+
+  Widget _loginPage() {
+    return FlutterLogin(
+        title: 'Login',
+        logo: 'assets/images/ecorp.png',
+        onSignup: _onSignup,
+        onLogin: _onLogin,
+        onRecoverPassword: null,
+        onSubmitAnimationCompleted: () {
+          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen(),));
+        },
+    );
+  }
+
+  Future<String> _onSignup(LoginData data) {
+    print('[onSignup] Name: ${data.name}, Password: ${data.password}');
+
+    return Future.delayed(loginTime).then((_) {
+      userMap.putIfAbsent(data.name, () => data.password);
+      final session = _generateSession(data.name, data.password);
+      _saveSession(session);
+      return null;
+    });
+  }
+
+  Future<String> _onLogin(LoginData data) {
+    print('[onLogin] Name: ${data.name}, Password: ${data.password}');
+
+    return Future.delayed(loginTime).then((_) {
+      if(!userMap.containsKey(data.name)) {
+        return 'Username not exists';
+      }
+
+      final session = _generateSession(data.name, data.password);
+      if(!_validSession(session)) {
+        return 'Password does not match';
+      }
+
+      _saveSession(session);
+      return null;
+    });
+  }
+
+  String _generateSession(String username, String password) {
+    return username + password;
+  }
+
+  void _saveSession(String session) async {
+    await _storage.write(key: sessionKey, value: session);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demo_flutter/login.dart';
+import 'package:demo_flutter/login/login.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,23 +9,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        accentColor: Colors.orange,
-        cursorColor: Colors.orange,
-        textTheme: TextTheme(
-          headline3: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 45.0,
-            color: Colors.orange
-          ),
-          button: TextStyle(
-            fontFamily: 'OpenSans',
-          ),
-          subtitle1: TextStyle(fontFamily: 'NotoSans'),
-          bodyText2: TextStyle(fontFamily: 'NotoSans'),
-        ),
-      ),
+      // theme: ThemeData(
+      //   primarySwatch: Colors.deepPurple,
+      //   accentColor: Colors.orange,
+      //   cursorColor: Colors.orange,
+      //   textTheme: TextTheme(
+      //     headline3: TextStyle(
+      //       fontFamily: 'OpenSans',
+      //       fontSize: 45.0,
+      //       color: Colors.orange
+      //     ),
+      //     button: TextStyle(
+      //       fontFamily: 'OpenSans',
+      //     ),
+      //     subtitle1: TextStyle(fontFamily: 'NotoSans'),
+      //     bodyText2: TextStyle(fontFamily: 'NotoSans'),
+      //   ),
+      // ),
       home: LoginScreen(),
       );
   }
