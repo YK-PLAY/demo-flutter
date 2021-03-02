5b16059db488c3e29d0122760263511b34e6b1f4
Kebron <ztkmkoo@gmail.com>
2021-03-02 22:48:25 +0900

    # fix login_bloc.dart bug

--- lib/login/login_bloc.dart
+++ lib/login/login_bloc.dart
@@ -18,17 +18,17 @@ class LoginBloc with ChangeNotifier {
   String get alert => _alert;
 
   void setPhone(String value) {
-    _phone = value;
-    notifyListeners();
-  }
-
-  void setDialCode(String value) {
     if(!RegExp(r"^(?:[+0]+)?[0-9]{6,14}$").hasMatch(value)) {
       _alert = LoginConstants.errorPhone;
     } else {
       _alert = LoginConstants.validPhoneMessage;
     }
 
+    _phone = value;
+    notifyListeners();
+  }
+
+  void setDialCode(String value) {
     _dialCode = value;
     notifyListeners();
   }
@@ -63,28 +63,36 @@ class LoginBloc with ChangeNotifier {
     final String url = 'http://' + config.host + '/api/v1/auth/register';
     print(url);
 
-    try {
-      final response = await http.post(
-        url,
-        headers: <String, String> {
-          'Content-Type': 'application/json; charset=UTF-8'
-        },
-        body: jsonEncode(<String, String> {
-          'username': _phone,
-          'uuid': 'asdf',
-        }),
-      );
+    final response = await http.post(
+      url,
+      headers: <String, String> {
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String> {
+        'username': _phone,
+        'uuid': 'asdf',
+      }),
+    ).timeout(
+        Duration(seconds: 1),
+        onTimeout: () {
+          print('Timeout');
+          return null;
+        }
+    );
 
-      final resMap = jsonDecode(response.body);
-      final int status = resMap['status'];
-      if(status == 0) {
-        success(context);
-      } else {
-        failure();
-      }
-    } catch(_) {
-      print('Network error!');
+    if(response == null) {
       failure();
+      setAlert(LoginConstants.errorSummit);
+      return;
+    }
+
+    final resMap = jsonDecode(response.body);
+    final int status = resMap['status'];
+    if(status == 0) {
+      success(context);
+    } else {
+      failure();
+      setAlert(LoginConstants.errorSummit);
     }
   }
 }
--- lib/login/login_constants.dart
+++ lib/login/login_constants.dart
@@ -2,6 +2,7 @@ class LoginConstants {
   // login bloc
   static const String errorPhone = "Phone number is incorrect";
   static const String validPhoneMessage = "";
+  static const String errorSummit = "Network error";
 
   // login screen
   static const String phoneInputHintText = "Please enter your phone";
